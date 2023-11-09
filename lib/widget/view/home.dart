f81f0c119136288056cc6b20188effec1d1c71f4
nono <arnaudjourdain111@gmail.com>
2023-11-09 23:32:16 +0100

    floating button

--- lib/widget/view/home.dart
+++ lib/widget/view/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_partiel_5al/bloc/state_status.dart';
 import 'package:flutter_partiel_5al/bloc/user_bloc/auth_bloc.dart';
 import 'package:flutter_partiel_5al/widget/drawer_profile/drawer_profile.dart';
 
@@ -11,7 +12,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   void initState() {
     super.initState();
@@ -26,7 +26,25 @@ class _MyHomePageState extends State<MyHomePage> {
         title: const Text('Home Page'),
       ),
       drawer: const DrawerProfile(),
-      body: Container(),
+      body: const Placeholder(),
+      floatingActionButton: BlocBuilder<AuthBloc, AuthState>(
+        builder: (context, state) {
+          return FloatingActionButton(
+            onPressed: () {
+              if (state.status.status != StateStatusEnum.success) {
+                Scaffold.of(context).openDrawer();
+              } else {
+
+              }
+            },
+            backgroundColor: const Color(0xFF626af7),
+            child: const Icon(
+              Icons.add,
+              color: Colors.white,
+            ),
+          );
+        },
+      ),
     );
   }
 }
