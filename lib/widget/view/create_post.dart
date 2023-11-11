15b336b6e15c24ee2f2f94ee73177201d27032b8
nono <arnaudjourdain111@gmail.com>
2023-11-11 17:55:33 +0100

    Clean loading alert

--- lib/widget/view/create_post.dart
+++ lib/widget/view/create_post.dart
@@ -3,7 +3,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_partiel_5al/bloc/post_management_bloc/post_management_bloc.dart';
 import 'package:flutter_partiel_5al/bloc/state_status.dart';
 import 'package:flutter_partiel_5al/model/image_picker_controller.dart';
-import 'package:flutter_partiel_5al/widget/alert/sending_post_alert.dart';
 import 'package:flutter_partiel_5al/widget/form/image_picker_field.dart';
 
 class CreatePost extends StatefulWidget {
@@ -17,29 +16,12 @@ class _CreatePostState extends State<CreatePost> {
   final ImagePickerController imagePickerController = ImagePickerController();
   final TextEditingController textController = TextEditingController();
 
-  void sendForm(BuildContext context) {
+  void sendForm() {
     final postManagementBloc = BlocProvider.of<PostManagementBloc>(context);
     postManagementBloc.add(CreatePostEvent(
       content: textController.text,
       image: imagePickerController.image,
     ));
-    showLoadingAlert(context, postManagementBloc);
-  }
-
-  void showLoadingAlert(BuildContext context, PostManagementBloc postManagementBloc) {
-    showDialog<String>(
-      context: context,
-      builder: (BuildContext context) =>
-          BlocProvider(
-            create: (BuildContext context) => postManagementBloc,
-            child: const SendingPostAlert(),
-          ),
-      barrierDismissible: false,
-    ).then((value) {
-      if (postManagementBloc.state.status.status == StateStatusEnum.success) {
-        Navigator.of(context).pop();
-      }
-    });
   }
 
   @override
@@ -50,43 +32,67 @@ class _CreatePostState extends State<CreatePost> {
       ),
       body: BlocBuilder<PostManagementBloc, PostManagementState>(
         builder: (context, state) {
-          return Padding(
-            padding: const EdgeInsets.only(
-              top: 16,
-              left: 16,
-              right: 16,
-            ),
-            child: Wrap(
-              runSpacing: 16,
-              children: [
-                TextField(
-                  controller: textController,
-                  maxLines: 8, //or null
-                  decoration: const InputDecoration(
-                    hintText: "Enter your message",
-                    fillColor: Colors.black12,
-                    filled: true,
-                    border: InputBorder.none,
-                  ),
+          if (state.status.status == StateStatusEnum.success) {
+            Navigator.pop(context);
+            return Container();
+          }
+          return Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 16,
+                  left: 16,
+                  right: 16,
                 ),
-                Center(
-                  child: Text(
-                    state.status.message ?? "",
-                    style: const TextStyle(
-                      color: Colors.red,
+                child: Wrap(
+                  runSpacing: 16,
+                  children: [
+                    TextField(
+                      controller: textController,
+                      maxLines: 8,
+                      decoration: const InputDecoration(
+                        hintText: "Enter your message",
+                        fillColor: Colors.black12,
+                        filled: true,
+                        border: InputBorder.none,
+                      ),
+                    ),
+                    Center(
+                      child: Text(
+                        state.status.message ?? "",
+                        style: const TextStyle(
+                          color: Colors.red,
+                        ),
+                      ),
+                    ),
+                    ImagePickerField(
+                      imagePickerController: imagePickerController,
+                    ),
+                  ],
+                ),
+              ),
+              state.status.status == StateStatusEnum.loading ? Container(
+                color: Colors.black54,
+              ) : Container(),
+              state.status.status == StateStatusEnum.loading ? const Center(
+                child: SizedBox(
+                  width: 300,
+                  height: 200,
+                  child: SizedBox(
+                    width: 75,
+                    height: 75,
+                    child: Center(
+                      child: CircularProgressIndicator(),
                     ),
                   ),
                 ),
-                ImagePickerField(
-                  imagePickerController: imagePickerController,
-                ),
-              ],
-            ),
+              ) : Container(),
+            ],
           );
         },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => sendForm(context),
+        onPressed: sendForm,
         backgroundColor: const Color(0xFF626af7),
         child: const Icon(
           Icons.send,
