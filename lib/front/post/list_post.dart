b34b47825ba1c8deeff44a27d2f101bea6e4519d
nono <arnaudjourdain111@gmail.com>
2024-02-15 15:02:31 +0100

    Refresh pages

--- lib/front/post/list_post.dart
+++ lib/front/post/list_post.dart
@@ -19,7 +19,6 @@ class ListPost extends StatefulWidget {
 }
 
 class _ListPostState extends State<ListPost> {
-
   final ScrollController _scrollController = ScrollController();
 
   @override
@@ -31,7 +30,9 @@ class _ListPostState extends State<ListPost> {
   void _scrollListener() {
     final postListBloc = BlocProvider.of<PostListBloc>(context);
 
-    if (_scrollController.position.extentAfter < 500 && postListBloc.state.status.status != StateStatusEnum.loading && postListBloc.state.status.status != StateStatusEnum.loadingNewItems) {
+    if (_scrollController.position.extentAfter < 500 &&
+        postListBloc.state.status.status != StateStatusEnum.loading &&
+        postListBloc.state.status.status != StateStatusEnum.loadingNewItems) {
       postListBloc.add(AddListPost());
     }
   }
@@ -46,7 +47,6 @@ class _ListPostState extends State<ListPost> {
           case StateStatusEnum.loading:
             return const Loading();
           case StateStatusEnum.success:
-          case StateStatusEnum.loadingNewItems:
             return RefreshIndicator(
               onRefresh: () async {
                 final postListBloc = BlocProvider.of<PostListBloc>(context);
@@ -56,6 +56,34 @@ class _ListPostState extends State<ListPost> {
                 controller: _scrollController,
                 itemCount: state.postList!.itemsReceived,
                 itemBuilder: (BuildContext context, int index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: PostItem(
+                      post: state.postList!.items![index],
+                      refreshFunction: widget.refreshListFunction,
+                    ),
+                  );
+                },
+              ),
+            );
+          case StateStatusEnum.loadingNewItems:
+            return RefreshIndicator(
+              onRefresh: () async {
+                final postListBloc = BlocProvider.of<PostListBloc>(context);
+                postListBloc.add(GetListPost());
+              },
+              child: ListView.builder(
+                controller: _scrollController,
+                itemCount: state.postList!.itemsReceived! + 1,
+                itemBuilder: (BuildContext context, int index) {
+                  if (index == state.postList!.itemsReceived!) {
+                    return const Padding(
+                      padding: EdgeInsets.all(32.0),
+                      child: Center(
+                        child: CircularProgressIndicator(),
+                      ),
+                    );
+                  }
                   return Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: PostItem(
